c9c4a061767173b42c9f89f16f9008ccac40484f
Muhammad Anas <muhd.anas@outlook.com>
2020-03-11 11:37:10 +0800

    Set up connection to DB.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the ContactKeeper API...' });
