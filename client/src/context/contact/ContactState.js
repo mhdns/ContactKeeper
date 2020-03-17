7fd524cf36af01e47516a734b4ac7f52aec7d2e4
Muhammad Anas <muhd.anas@outlook.com>
2020-03-17 22:03:29 +0800

    Added Context for contacts.

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,78 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types'
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id:1,
+        name:'Jackie',
+        email: 'J@gmail.com',
+        phone: '91238329',
+        type: 'professional'
+      },
+      {
+        id:2,
+        name:'Jillie',
+        email: 'Jill@gmail.com',
+        phone: '81938329',
+        type: 'personal'
+      },
+      {
+        id:3,
+        name:'Nashie',
+        email: 'Nashie@gmail.com',
+        phone: '81728283',
+        type: 'personal'
+      },
+      {
+        id:4,
+        name:'Blackie',
+        email: 'blakie@gmail.com',
+        phone: '91238322',
+        type: 'professional'
+      }
+    ]
+  }
+
+  const [state, dispatch] = useReducer(contactReducer, initialState)
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+  
+  // Filter Contacts
+
+  // Clear Filter
+  
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  )
+}
+
+
+
+export default ContactState;
