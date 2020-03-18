767362fbca5b85d856d34925cafd2900166b6ca5
Muhammad Anas <muhd.anas@outlook.com>
2020-03-18 20:30:25 +0800

    Added addContact function in ContactState and contactReducer.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -49,7 +49,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState)
 
   // Add Contact
-
+  const addContact = contact => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  };
   // Delete Contact
 
   // Set Current Contact
@@ -65,7 +68,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact
       }}
     >
       {props.children}
