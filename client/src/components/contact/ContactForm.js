8358e4b86ff994bcc182ec63ba9f88b3fdfc3683
Muhammad Anas <muhd.anas@outlook.com>
2020-03-19 21:26:53 +0800

    Implement Clear button, made the title and submit button change when in edit "mode".

--- client/src/components/contact/ContactForm.js
+++ client/src/components/contact/ContactForm.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react'
 import ContactContext from '../../context/contact/contactContext';
+import { CLEAR_CURRENT } from '../../context/types';
 
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
 
-  const { addContact, current } = contactContext;
+  const { addContact, current, clearCurrent } = contactContext;
 
   useEffect(() => {
     if(current !== null) {
@@ -23,6 +24,16 @@ const ContactForm = () => {
 
   const onChamge = e => setContact({...contact, [e.target.name]: e.target.value});
 
+  const clearAll = () => {
+    clearCurrent();
+    setContact({
+      name:'',
+      phone:'',
+      email:'',
+      type:'personal'
+    })
+  }
+
   const onSubmit = e => {
     e.preventDefault();
     addContact(contact);
@@ -35,7 +46,7 @@ const ContactForm = () => {
   }
   return (
     <form onSubmit={onSubmit}>
-      <h2 className='text-primary'>Add Contact</h2>
+      <h2 className='text-primary'>{current ? 'Update Contact' : 'Add Contact'}</h2>
       <input type='text' placeholder='Name' name='name' value={name} onChange={onChamge}/>
       <input type='email' placeholder='Email' name='email' value={email} onChange={onChamge}/>
       <input type='text' placeholder='Phone' name='phone' value={phone} onChange={onChamge}/>
@@ -46,8 +57,14 @@ const ContactForm = () => {
       {' '}{'Professional'}{' '}
 
       <div>
-        <input type='submit' value='Add Contact' className='btn btn-ptimary btn-block'/>
+        <input type='submit' value={current ? 'Update Contact' : 'Add Contact'} className='btn btn-primary btn-block'/>
       </div>
+      {current && 
+        <div>
+          <button className='btn btn-light btn-block' onClick={clearAll}>
+            Clear
+          </button>
+        </div>}
     </form>
   )
 }
