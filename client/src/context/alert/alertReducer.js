0799f09d7c40755c23b2bce98059ab21d16dff53
Muhammad Anas <muhd.anas@outlook.com>
2020-03-21 16:54:34 +0800

    Alert component implemented and brought into App.js.

--- client/src/context/alert/alertReducer.js
+++ client/src/context/alert/alertReducer.js
@@ -8,7 +8,7 @@ export default (state, action) => {
     case SET_ALERT:
       return [...state, action.payload];
     case REMOVE_ALERT:
-      return state.filter(alert => alert.id !== payload.id)
+      return state.filter(alert => alert.id !== action.payload.id)
     default:
        return state;
   }
