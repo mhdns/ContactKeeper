6d94e89daaa0d68ca892e58bd1b32c880c0e89b3
Muhammad Anas <muhd.anas@outlook.com>
2020-03-25 13:34:03 +0800

    Prepared for deployment.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -11,15 +12,20 @@ connectDB();
 // Init Middleware (Alternative to bodyParser from express)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the ContactKeeper API...' });
-});
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts/:id', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
